bbb9d0c8c14af11e9048be437cfcfe7a6bc45a10
Jrevata <jordan.revata@tecsup.edu.pe>
2018-04-06 13:12:08 -0500

    Initial commit

---
+++ app/src/main/java/com/fragmentsapp/jrevata/fragmentsexampleapp/SecondFragment.java
@@ -0,0 +1,50 @@
+package com.fragmentsapp.jrevata.fragmentsexampleapp;
+
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Alumno on 6/04/2018.
+ */
+
+public class SecondFragment extends Fragment {
+
+    private static final String TAG = SecondFragment.class.getSimpleName();
+
+    private String param1;
+
+    public SecondFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        // Get argument param1 if exists
+        if (getArguments() != null) {
+            this.param1 = getArguments().getString("param1");
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        TextView textView = (TextView)view.findViewById(R.id.textview);
+        textView.setText(textView.getText() + "\nParam1: " + param1);
+
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/fragmentsapp/jrevata/fragmentsexampleapp/ThirdFragment.java
@@ -0,0 +1,58 @@
+package com.fragmentsapp.jrevata.fragmentsexampleapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Alumno on 6/04/2018.
+ */
+
+public class ThirdFragment extends Fragment {
+
+    // Nombre de parámetro declarado como constante
+    private static final String ARG_PARAM1 = "param1";
+
+    // Atributo param1
+    private String param1;
+
+    public ThirdFragment() {
+
+    }
+
+    // Como buena práctica definimos un método fáctory 'newInstance' que reciba los parámetros requeridos 'param1'
+    public static ThirdFragment newInstance(String param1) {
+
+        ThirdFragment fragment = new ThirdFragment();
+
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            param1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+
+        TextView textView = (TextView)view.findViewById(R.id.textview);
+        textView.setText(textView.getText() + "\nParam1: " + param1);
+
+        return view;
+    }
+
+}
